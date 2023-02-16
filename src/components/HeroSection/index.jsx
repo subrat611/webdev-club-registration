c93bec2c7ce2909675c30c7e86da5d5fce6d2960
subrat <subratj611@gmail.com>
2023-02-17 00:46:47 +0530

    hero section done

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,25 @@
+import "./herosection.scss";
+
+export default function HeroSection() {
+  return (
+    <div className="hero-wrapper">
+      <div className="hero-inner-wrapper">
+        <div>
+          <span className="hero-title">Students at Opju</span>
+          <br />
+          <span className="hero-title">
+            are <span className="hero-title-bg-style">invited</span>
+          </span>
+          <br />
+          <span className="hero-title">
+            to join a{" "}
+            <span className="hero-title-bg-style">web developers club</span>
+          </span>
+          <br />
+          <span className="hero-desc">It is never too late to start...</span>
+        </div>
+        <button className="hero-action-btn">join the club</button>
+      </div>
+    </div>
+  );
+}
