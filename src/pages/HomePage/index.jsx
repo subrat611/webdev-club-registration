9f35a97959d00023bff6839b23357455db9b1d72
subrat <subratj611@gmail.com>
2023-02-17 00:47:07 +0530

    home page update

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../../components/HeroSection";
+import NavBar from "../../components/NavBar";
+
+export default function HomePage() {
+  return (
+    <div className="homepage-wrapper">
+      <NavBar />
+      <HeroSection />
+    </div>
+  );
+}
