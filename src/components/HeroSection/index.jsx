df34a6e5539afb206af1410de303ba8b91828ac5
subrat <subratj611@gmail.com>
2023-02-18 15:40:27 +0530

    all changes done

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -5,18 +5,18 @@ export default function HeroSection({ scrollToSection }) {
     <div className="hero-wrapper">
       <div className="hero-inner-wrapper">
         <div className="hero-title-container">
-          <span className="hero-title">Students at Opju</span>
+          <span className="hero-title">Students at Opju,</span>
           <br />
           <span className="hero-title">
-            <span className="hero-title-bg-style">invited</span>{" "}
-            <span className="hero-title">to join a </span>
+            <span className="hero-title-bg-style">we invite you</span>{" "}
+            <span className="hero-title">to join</span>
           </span>
           <br />
           <span className="hero-title hero-title-bg-style">
-            developers club
+            our developers club
           </span>
           <br />
-          <span className="hero-desc">It is never too late to start...</span>
+          <span className="hero-desc">It's never too late to start...</span>
         </div>
         <button className="hero-action-btn" onClick={scrollToSection}>
           join the club
